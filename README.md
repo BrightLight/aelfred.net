40666073c94bad8fc34544370bb9908bb8db0db8
Rasmus John Pedersen <mail@rjp.dk>
2014-07-12 00:56:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Ælfred.Net
+
+Ælfred.Net is a .NET port of the Ælfred (http://saxon.sourceforge.net/aelfred.html) XML Parser.
+
+Ælfred.Net is SAX2-compatible a non-validating XML parser.
+
+# Configuration
+
+To use Ælfred.Net set `xmlReaderFactoryType` to `AELFred.Net.XmlReaderFactory, AElfred.Net`
+
+``` XML
+<configSections>
+  <section name="sax" type="Sax.Net.Helpers.SaxConfigurationSection, Sax.Net"/>
+</configSections>
+<sax xmlReaderFactoryType="AELFred.Net.XmlReaderFactory, AElfred.Net"/>
+```
+
+# Usage
+
+See [SAX.Net](http://www.github.com/rasmusjp/sax.net#usage)
+
+
+# License
+
+Ælfred.Net is licensed under [LGPL V3](LICENSE).
+
+Microstar's (which no longer exists) original terms and conditions:
+
+AElfred, Version 1.2
+Microstar's Java-Based XML Parser
+Copyright (c) 1997, 1998 by Microstar Software Ltd.
+Home Page: http://www.microstar.com/XML/
+
+AElfred is free for both commercial and non-commercial use and redistribution, provided that Microstar's copyright and disclaimer are retained intact. You are free to modify AElfred for your own use and to redistribute AElfred with your modifications, provided that the modifications are clearly documented.
+
+DISCLAIMER
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of merchantability or fitness for a particular purpose. Please use it AT YOUR OWN RISK.
---
+++ src/AElfred/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ælfred.Net")]
+[assembly: AssemblyDescription("Parse XML documents and return parse events through call-backs.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rasmus John Pedersen")]
+[assembly: AssemblyProduct("Ælfred.Net")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2c4a4ea7-8fef-42be-b2b7-96a83d1c7d09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: CLSCompliant(true)]
